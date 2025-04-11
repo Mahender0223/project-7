573b6964b9e71fac27b8acca3d83fcc3e0689406
Mahender0223 <mahender0223@gmail.com>
2025-04-11 11:25:37 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+    <>
+      <div>
+       <p>Hello World</p>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
